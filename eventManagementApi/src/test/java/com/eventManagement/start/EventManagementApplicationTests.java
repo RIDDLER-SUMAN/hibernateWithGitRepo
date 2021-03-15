a0f590c707704a5966433fced5d12dcc6826747b
THERIDDLER <pati.suman01@gmail.com>
2021-03-16 01:18:34 +0530

    Commit for the first time

---
+++ eventManagementApi/src/test/java/com/eventManagement/start/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventManagement.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
